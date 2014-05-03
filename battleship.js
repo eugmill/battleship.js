833d4b19a5f0ddb55dcf91cab21c167b4a1f4564
Eugene Millerman <e.v.millerman@gmail.com>
2014-05-02 20:08:33 -0400

    Add coordinates

--- battleship.js
+++ battleship.js
@@ -1,4 +1,5 @@
-function Board(){
+function Board(placements){
+  this.placements = placements
   this.shots = []
   this.hits = []
   this.misses = []
@@ -8,5 +9,21 @@ Board.prototype.toString = function(){
   return "This is a board!";
 }
 
-var b = new Board();
-console.log(b.toString());
+function Placement(size,coords,orientation){
+  this.size = size;
+  this.coords = coords;
+  if(orientation== 'down' || orientation== 'right') {
+    this.orientation = orientation
+  } else {
+    throw "Invalid orientation" 
+  }
+}
+
+function Coordinate(row,col) {
+  this.row = row;
+  this.col = col;
+}
+
+var c = new Coordinate(0,0);
+var placement = new Placement(5,c,'right')
+var b = new Board([placement]);
